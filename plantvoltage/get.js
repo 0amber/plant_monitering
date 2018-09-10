9eecde4774768c035d3f9c6bfe9ae77e98db0473
root <root@instance-2.c.responsive-city-210108.internal>
2018-09-10 08:55:30 +0000

    replace to mongodb atlas

--- plantvoltage/get.js
+++ plantvoltage/get.js
@@ -8,22 +8,22 @@ var fs = require("fs");
 const path  = require('path')
 const iconv = require('iconv-lite')
 var cons = require('consolidate');
-//var cors = require('cors');
-var corser = require('corser');
 server.listen(8000);
 
 //use static file
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/view'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //use html as template
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-// allow CORS not yet conpleated
-//app.use(cors());
-app.use(corser.create());
 //DB setup
 var Schema   = mongoose.Schema;
 var MonitorSchema = new Schema({
@@ -34,19 +34,30 @@ var MonitorSchema = new Schema({
   time:Number
 });
 mongoose.model('Monitor', MonitorSchema);
-mongoose.connect('mongodb://localhost:27017/monitor',{ useNewUrlParser: true });
+const dbURI =
+"mongodb+srv://<user>:<pass>@cluster0-twpop.gcp.mongodb.net/test?retryWrites=true";
+const options = {
+  useNewUrlParser: true
+};
 
+mongoose.connect(dbURI, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  err => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
 //send data to other browsers
 io.on('connection', (socket) => {
   socket.on('received', (msg) => {
-    socket.broadcast.emit("moniterVal", msg);
     io.sockets.emit("moniterVal", msg);
+    socket.broadcast.emit("moniterVal", msg);
   });
 });
 
 //RealtimeMonitor page
 app.get(`/`, (req, res) => {
-
   // request parameter from arduino
   moisture = req.query.val;
   volt = req.query.volt;
@@ -56,11 +67,9 @@ app.get(`/`, (req, res) => {
   var moniterVal = { moisture:moisture, volt:volt, temperature:temperature, illuminance:illuminance};
   console.log('moisture set to '+moisture + ' volt set to '+ volt + ' temperature set to '+ temperature + ' illuminance set to '+ illuminance);
 
-  //send to realtime monitior
-  io.sockets.emit("moniterValPre", moniterVal);
 
   //push data to MongoDB
-/*  var Monitor = mongoose.model('Monitor');
+  var Monitor = mongoose.model('Monitor');
   var monitor = new Monitor();
   monitor.volt = volt;
   monitor.moisture = moisture;
@@ -69,21 +78,38 @@ app.get(`/`, (req, res) => {
   monitor.time = now;
   monitor.save(function(err) {
     if (err) { console.log(err); } else {
-  //    console.log('save success!');
+      console.log('save success!');
     }
   });
-*/
-  //render to html
-  var html = fs.readFileSync("./index4.html", "utf-8");
-  res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
-  res.end(html);
+
+  Monitor.find({},{},{limit:20},(err,monitorData)=>{
+    var volts = [];
+    var moistures = [];
+    var temperatures = [];
+    var illuminances =[];
+    var times = [];
+    for (var i = 0; i < monitorData.length; i++) {
+      var volt = monitorData[i].volt;
+      var moisture = monitorData[i].moisture;
+      var temperature = monitorData[i].temperature;
+      var illuminance = monitorData[i].illuminance;
+      var time = monitorData[i].time;
+      volts.push(volt);
+      moistures.push(moisture);
+      temperatures.push(temperature);
+      illuminances.push(illuminance);
+      times.push(time);
+    }
+    res.render('index4', {volts:volts, moistures:moistures, temperatures:temperatures, illuminances:illuminances, times:times});
+  });
+
 });
 
 //Plot Analys page
 app.get(`/plot`, (req, res) => {
   var Monitor = mongoose.model('Monitor');
 //  var monitor = new Monitor();
-  Monitor.find({},{lomit:10000}, function(err, monitorData){
+  Monitor.find({},function(err, monitorData){
     //render to html
     var volts = [];
     var moistures = [];
@@ -106,3 +132,4 @@ app.get(`/plot`, (req, res) => {
   });
 
 });
+
